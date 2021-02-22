e70c537ecd400251e5a3ca9ea78f6c70d1d3629c
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-02-22 15:11:14 +0200

    main update

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from AutoTasks import AutomaticTasks, start_tasks
 from DiscordBot import DiscordBot
 from xcash_wallet.xcash import XcashManager
 from backendManager.backend import BackendManager
+from utils.tools import Helpers
 
 if __name__ == "__main__":
+    helper = Helpers()
+    bot_settings = helper.read_json_file(file_name='botSetup.json')
     backend_manager = BackendManager()
     print("Checking required collections")
     backend_manager.integrity_check.check_collections()
-    dpops_wrapper = Dpops()
+    dpops_wrapper = Dpops(dpops_api=bot_settings["dpopsApi"])
     xcash_manager = XcashManager()
     bot = DiscordBot(dpops_wrapper, xcash_manager, backend_manager)
     auto_tasks = AutomaticTasks(dpops_wrapper, bot)
