9845d76d1023a065f7abf0429289031c1858778f
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-02-22 13:32:56 +0200

    some documentation update

---
+++ AutoTasks.py
@@ -0,0 +1,138 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from cogs.utils.customMessages import global_stats, top_delegates
+from operator import itemgetter
+from discord import Color, Embed
+from pprint import pprint
+
+from datetime import datetime
+import json
+
+
+class AutomaticTasks:
+    def __init__(self, dpops_wrapper, bot):
+        self.dpops_wrapper = dpops_wrapper
+        self.bot = bot
+        self.xDelegate = self.bot.dpops_queries.xpayment
+        self.new_block_channel = 812299916960464936
+
+    def __store_last_updated_block_height(self, block_height):
+        """
+        Stores the height of last block which was monitored for incoming transactions
+        :param block_height: block height from RPC Daemon as INT
+        :return: Updates the value in last_block.json
+        """
+        try:
+            data = {"block_height": block_height}
+
+            with open('last_block.json', 'w') as f:
+                json.dump(data, f)
+
+            return True
+        except Exception:
+            return False
+
+    def __read_last_checked_block_data(self):
+        """
+        Loads the last block height which was stored in last_block.json
+        :return: Block height as INT
+        """
+
+        with open('last_block.json') as json_file:
+            data = json.load(json_file)
+            last_marked_blocked = data["block_height"]
+            return last_marked_blocked
+
+    async def send_dpops_stats(self):
+        try:
+            print('Getting global stats')
+            data = self.bot.dpops_queries.statistics.global_stats()
+            delegates_count = len(self.bot.dpops_queries.delegates.get_delegates())
+            data["totalDelegates"] = delegates_count
+            stats_chn = self.bot.get_channel(id=self.bot.stats_channel_id)
+            if not data.get("error"):
+                await global_stats(destination=stats_chn, data=data)
+            else:
+                error = data["error"]
+                print(error)
+        except Exception:
+            print("Error")
+
+    def get_round_stats(self):
+        # Store somewhere block
+        # Block details retur
+        pass
+
+    def block_monitor(self):
+        # Block monitor
+        pass
+
+    async def delegate_ranks(self):
+        all_delegates = self.bot.dpops_queries.delegates.get_delegates()
+        print("getting delegates")
+        if isinstance(all_delegates, list):
+            for delegate in all_delegates:
+                delegate["total_vote_count"] = int(delegate["total_vote_count"])
+
+            newlist = sorted(all_delegates, key=itemgetter('total_vote_count'), reverse=True)
+
+            top_10 = newlist[0:10]
+
+            stats_chn = self.bot.get_channel(id=self.bot.stats_channel_id)
+            await top_delegates(destination=stats_chn, c=Color.dark_orange(), data=top_10)
+        else:
+            error = all_delegates["error"]
+            print(error)
+
+    async def xpayment_block_checker(self):
+        print('Checking found blocks')
+        last_checked_block = self.__read_last_checked_block_data()
+        last_block_found = self.xDelegate.get_last_block_found()[0]
+
+        last_produced_block = int(last_block_found["block_height"])
+
+        if last_produced_block > last_checked_block:
+            print("block found")
+            block_channel = self.bot.get_channel(id=self.new_block_channel)
+
+            new_block = Embed(title=f':bricks: New block',
+                              description=f'Height @ ***{int(last_block_found["block_height"]):,}***',
+                              colour=Color.green())
+            new_block.add_field(name=f":date: Time",
+                                value=f"```{datetime.fromtimestamp(int(last_block_found['block_date_and_time']))}```")
+            new_block.add_field(name=f":moneybag: Block Value",
+                                value=f"```{float(last_block_found['block_reward']) / (10 ** 6):,} XCASH```",inline=False)
+
+            await block_channel.send(embed=new_block)
+
+            self.__store_last_updated_block_height(block_height=int(last_produced_block))
+        else:
+            print(f"No new blocks have been found by delegate @ f{datetime.utcnow()}")
+
+    async def notify_on_cashout(self):
+        pass
+
+
+def start_tasks(automatic_tasks):
+    """
+    Starts all tasks in the backgroudn
+    :param automatic_tasks: AutomaticTasks class
+    :return: scheduler
+    """
+    scheduler = AsyncIOScheduler()
+    print('Started Chron Monitors')
+    # scheduler.add_job(automatic_tasks.block_monitor,CronTrigger(hour='02',second='02'), misfire_grace_time=2,
+    #                   max_instances=20)
+    #
+    # scheduler.add_job(automatic_tasks.send_dpops_stats,
+    #                   CronTrigger(minute='00'), misfire_grace_time=2, max_instances=20)
+    #
+    # scheduler.add_job(automatic_tasks.delegate_ranks, CronTrigger(hour='02', second='02'), misfire_grace_time=2,
+    #                   max_instances=20)
+    scheduler.add_job(automatic_tasks.xpayment_block_checker, CronTrigger(minute='02,04,06,08,10,12,14,16,18,20,22,24,26,'
+                                                                                 '28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58'), misfire_grace_time=2,
+                      max_instances=20)
+
+    scheduler.start()
+    print('Started Chron Monitors : DONE')
+    return scheduler
--- README.md
+++ README.md
@@ -1,2 +1,111 @@
-# dpops_bot
-Informational Discord Bot for DPOPS delegates 
+# DPOPS Monitor For Delegates
+
+# About
+Discord Bot monitor which can be deployed by anyone on their private server. 
+
+
+# Integrated functions
+
+- [x] Voter 
+    - [x] Management of own voter profile
+        - [x] Apply public key for easier queries presented below
+    - [x] Voter queries with or without registered profile
+        - [x] Last 4 payments sent by delegate for voter
+        - [x] Current state of the voter in delegate
+
+- [x] Server owner
+    - [x] block tracking
+        - [x] Register for automatic notifications when delegate produces new block
+    - [x] DPOPS Stats
+        - [ ] Daily overall statistical snapshots of delegate
+
+- [ ] X-Cash DPOPS network queries
+    
+
+# Installation
+Note: Following installation steps assume that you are on a Linux environment.
+
+## 1. Clone repo 
+- **Update the system**
+
+```shell script
+$ sudo apt update
+```
+
+- **Clone this repo to your local machine using**
+
+```shell script
+$ git clone https://github.com/AnimusXCASH/dpops_bot.git
+```
+
+
+## 2. Install requirements:
+
+### 2.1. Mongo Database
+
+[Mongo Installation Instructions](https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/)
+
+when installation completed 
+
+```shell script
+$ sudo service mongod start
+```
+
+### 2.2. PIP installation 
+[pip3/pip installation instructions](https://pip.pypa.io/en/stable/installing/) (Mandatory)**
+```shell script
+$ sudo apt install -y python3-pip
+$ pip3 --version
+```
+
+
+### 2.3. Install package requirements for project:
+
+- Install build tools  
+
+```shell script 
+sudo apt-get install build-essential libssl-dev libffi-dev python3-dev
+```
+
+-  **CD to main project folder and install requirements.. this will install all required packages for x-payment to function
+optimally (Mandatory)**
+```shell script
+$ pip3 install -r requirements.txt 
+```
+
+
+### 2.4. Discord part 
+#### 2.4.1. Register developer account required for bot to operate
+- [Register Discord developer account](https://discordapp.com/developers)
+- Create application and obtain bot token and ID ([How to](https://www.writebots.com/discord-bot-token/))
+- Create Bot Invite link with with [Discord Permission Calculator](https://discordapi.com/permissions.html#0)
+
+```text
+# Permission code when creating invitation link
+359489
+```
+
+- Invite bot to the community by clicking on link created through previous step
+    - All necessary permissions will be set automatically once bot joins
+
+#### 2.5. Bot Setup
+
+go to main project folder and create file name:
+```text
+botSetup.json
+```
+
+with content:
+```json
+{
+  "command": "COMMAND CHARACTER",
+  "token": "BOT TOKEN OBTAINED IN STEP 2.4.1 ",
+}
+
+Example:
+{
+  "command": "!",  // Bot will listen to command string ! or @botTag
+  "token": "cdscdscds.YAJ5uw.dockasmsdcasdcdascdasca",   // Bot token 
+}
+```
+
