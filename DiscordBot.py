7f3caae569104982bd3ca9c7566090d13d0dc359
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-02-24 13:30:46 +0200

    snaphost management command to load on start

--- DiscordBot.py
+++ DiscordBot.py
@@ -4,7 +4,7 @@ from discord import Intents
 from utils.tools import Helpers
 
 cogs_to_load = ["cogs.help", 'cogs.autoFunctions','cogs.botSetup',
-                "delegate.delegateCommands", "delegate.voterCommands"]
+                "delegate.delegateCommands", "delegate.voterCommands","cogs.snapshotSetupsDelegate"]
 
 
 class DiscordBot(commands.Bot):
