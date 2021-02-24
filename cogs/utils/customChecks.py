266df9ff9605c9c583ff15d7ddb2515316695a3b
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-02-24 14:58:55 +0200

    exception handling for custom checks

--- cogs/utils/customChecks.py
+++ cogs/utils/customChecks.py
@@ -2,8 +2,14 @@ from discord import ChannelType
 
 
 def is_owner(ctx):
-    return int(ctx.message.author.id) == int(ctx.message.guild.owner.id)
+    try:
+        return int(ctx.message.author.id) == int(ctx.message.guild.owner.id)
+    except AttributeError:
+        return False
 
 
 def is_public(ctx):
-    return ctx.message.channel.type != ChannelType.private
+    try:
+        return ctx.message.channel.type != ChannelType.private
+    except AttributeError:
+        return False
