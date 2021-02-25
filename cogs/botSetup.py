ebdfdd57597c37223e3011ab423964f9de86f251
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-02-25 10:55:03 +0200

    block monitor additional check if user forgets to set height or channel

--- cogs/botSetup.py
+++ cogs/botSetup.py
@@ -24,7 +24,7 @@ class BotSetup(commands.Cog):
         await customMessages.embed_builder(ctx=ctx, c=Colour.green(), title=title, description=description,
                                            list_of_commands=list_of_commands)
 
-    @commands.group()
+    @commands.group(aliases=["block"])
     @commands.check(is_public)
     @commands.check(is_owner)
     async def blocks(self, ctx):
@@ -48,7 +48,7 @@ class BotSetup(commands.Cog):
 
     @blocks.command()
     async def height(self, ctx, block_height: int):
-        if self.bot.bot_settings_manager.update_settings_by_dict(setting_name="new_block",
+        if self.bot.setting.update_settings_by_dict(setting_name="new_block",
                                                                  value={"value": int(block_height)}):
             await customMessages.system_message(ctx=ctx, c=Colour.green(), title="Block Height Setup",
                                                 error_details=f"You have successfully set starting block height to "
@@ -63,7 +63,7 @@ class BotSetup(commands.Cog):
 
     @blocks.command()
     async def chn(self, ctx, channel: TextChannel):
-        if self.bot.bot_settings_manager.update_settings_by_dict(setting_name="new_block",
+        if self.bot.setting.update_settings_by_dict(setting_name="new_block",
                                                                  value={"channel": int(channel.id)}):
             await customMessages.system_message(ctx=ctx, c=Colour.green(), title="Block Channel Setup",
                                                 error_details=f"You have successfully set channel ***{channel}*** "
@@ -78,30 +78,41 @@ class BotSetup(commands.Cog):
                                                 destination=ctx.channel)
 
     @blocks.command()
-    async def monitor(self, ctx, status_type: str):
-        if status_type.lower() in ['on', 'off']:
-            if status_type.lower() == "on":
-                message = "You have successfully turned ***ON*** block monitor."
-                status_code = 1
-            else:
-                message = "You have successfully turned ***OFF*** block monitor"
-                status_code = 0
+    async def monitor(self, ctx, status_type:str):
+        current = self.bot.setting.get_setting(setting_name='new_block')
+        if current["channel"] > 0 and current["value"] > 0:
+            if status_type.lower() in ['on', 'off']:
+                if status_type.lower() == "on":
+                    message = "You have successfully turned ***ON*** block monitor."
+                    status_code = 1
+                else:
+                    message = "You have successfully turned ***OFF*** block monitor"
+                    status_code = 0
 
-            if self.bot.bot_settings_manager.update_settings_by_dict(setting_name="new_block",
-                                                                     value={"status": int(status_code)}):
-                await customMessages.system_message(ctx=ctx, c=Colour.green(), title="Monitoring Status",
-                                                    error_details=message,
-                                                    destination=ctx.channel)
+                if self.bot.setting.update_settings_by_dict(setting_name="new_block",
+                                                                         value={"status": int(status_code)}):
+                    await customMessages.system_message(ctx=ctx, c=Colour.green(), title="Monitoring Status",
+                                                        error_details=message,
+                                                        destination=ctx.channel)
+                else:
+                    await customMessages.system_message(ctx=ctx, c=Colour.red(), title="System Error",
+                                                        error_details="It seems like there haas been a backend issue."
+                                                                      " Please try again later or open github ticket and "
+                                                                      "let us know.",
+                                                        destination=ctx.channel)
             else:
-                await customMessages.system_message(ctx=ctx, c=Colour.red(), title="System Error",
-                                                    error_details="It seems like there haas been a backend issue."
-                                                                  " Please try again later or open github ticket and "
-                                                                  "let us know.",
+                await customMessages.system_message(ctx=ctx, c=Colour.red(), title="Wrong status provided",
+                                                    error_details=f"Allowed status types are either ***ON*** or ***OFF***. You "
+                                                                  f"have provided {status_type}",
                                                     destination=ctx.channel)
         else:
-            await customMessages.system_message(ctx=ctx, c=Colour.red(), title="Wrong status provided",
-                                                error_details=f"Allowed status types are either ***ON*** or ***OFF***. You "
-                                                              f"have provided {status_type}",
+            await customMessages.system_message(ctx=ctx, c=Colour.red(), title="Set-up incomplete",
+                                                error_details=f"You have not set both of the required properties. "
+                                                              f"Channel and starting block height need to be provided"
+                                                              f" first before you can activate block monitor.\n"
+                                                              f" Current Status:"
+                                                              f"Block Height: {current['value']}\n"
+                                                              f"Channel: {current['channel']}",
                                                 destination=ctx.channel)
 
     @owner.error
