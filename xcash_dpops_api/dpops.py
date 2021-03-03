75bbc0e195fec6c540bc52946e464307d8bd4552
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-03-03 12:54:40 +0200

    calculator class to main class Dpops

--- xcash_dpops_api/dpops.py
+++ xcash_dpops_api/dpops.py
@@ -1,6 +1,7 @@
 from xcash_dpops_api.delegateApi import DelegateApiAccess
 from xcash_dpops_api.foundation import DpopsDelegates
 from xcash_dpops_api.xcashExplorer import XcashExplorer
+from xcash_dpops_api.dpopsCalculators import DpopsCalculator
 
 
 class Dpops:
@@ -8,8 +9,9 @@ class Dpops:
     Wrapper for various APIs related to delegates and xcash
     """
 
-    def __init__(self, dpops_api):
+    def __init__(self, dpops_api, delegate_name: str):
         self.foundation = "http://delegates.xcash.foundation/"
         self.delegate_api = DelegateApiAccess(dpops_api=dpops_api)
         self.delegates = DpopsDelegates(self.foundation)
         self.xcash_explorer = XcashExplorer()
+        self.calculator = DpopsCalculator(delegate_name=delegate_name)
