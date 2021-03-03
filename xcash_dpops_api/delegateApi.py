cf1ab87fab7bb02a78201339927565b836d932ee
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-03-03 15:02:13 +0200

    Script format

--- xcash_dpops_api/delegateApi.py
+++ xcash_dpops_api/delegateApi.py
@@ -3,7 +3,7 @@ import requests
 
 class DelegateApiAccess:
     def __init__(self, dpops_api=None):
-        self.api =  "http://xpayment.x-network.eu"
+        self.api = "http://xpayment.x-network.eu"
         self.statistics = '/shareddelegateswebsitegetstatistics'
         self.blocks_found = '/getblocksfound'
         self.public_address_info = '/getpublicaddressinformation'
@@ -49,5 +49,3 @@ class DelegateApiAccess:
         """
         endpoint = self.public_address_payment_info + f"?public_address={public_address}"
         return self.__process_request(api_link=endpoint)
-
-print(DelegateApiAccess().get_last_block_found())
