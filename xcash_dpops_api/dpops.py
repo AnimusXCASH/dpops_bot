8730d728ca0f001cc6ef7751ba721fb0aefeb1d1
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-03-03 12:27:52 +0200

    Additional api endpoints and classes

--- xcash_dpops_api/dpops.py
+++ xcash_dpops_api/dpops.py
@@ -1,7 +1,15 @@
 from xcash_dpops_api.delegateApi import DelegateApiAccess
+from xcash_dpops_api.foundation import DpopsDelegates
+from xcash_dpops_api.xcashExplorer import XcashExplorer
 
 
 class Dpops:
-    def __init__(self,dpops_api):
-        print(f'Dpops Api: {dpops_api}')
+    """
+    Wrapper for various APIs related to delegates and xcash
+    """
+
+    def __init__(self, dpops_api):
+        self.foundation = "http://delegates.xcash.foundation/"
         self.delegate_api = DelegateApiAccess(dpops_api=dpops_api)
+        self.delegates = DpopsDelegates(self.foundation)
+        self.xcash_explorer = XcashExplorer()
