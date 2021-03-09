af03a3745ffd22c554f7603e9d2b66f68798f8c6
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-03-09 14:02:37 +0200

    Automatic notifications on payments

--- cogs/autoFunctions.py
+++ cogs/autoFunctions.py
@@ -8,25 +8,25 @@ class AutoFunctions(commands.Cog):
         self.bot = bot
         self.command_string = self.bot.get_command_str()
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, exception):
-        """
-        Global error for on command error
-        """
-        try:
-            await ctx.message.delete()
-        except Exception:
-            pass
-
-        if isinstance(exception, commands.CommandNotFound):
-            title = '__Command Error__'
-            message = f'Command `{ctx.message.content}` is not implemented/active yet or it does not exist! Please' \
-                      f'type `{self.command_string}help` to check available commands.'
-            await customMessages.system_message(ctx=ctx, c=Colour.red(), title=title, error_details=message)
-
-        elif isinstance(exception, commands.MissingRequiredArgument):
-            await customMessages.system_message(ctx=ctx, c=Colour.red(), title=f'Missing required argument',
-                                                error_details=f'{exception}')
+    # @commands.Cog.listener()
+    # async def on_command_error(self, ctx, exception):
+    #     """
+    #     Global error for on command error
+    #     """
+    #     try:
+    #         await ctx.message.delete()
+    #     except Exception:
+    #         pass
+    #
+    #     if isinstance(exception, commands.CommandNotFound):
+    #         title = '__Command Error__'
+    #         message = f'Command `{ctx.message.content}` is not implemented/active yet or it does not exist! Please' \
+    #                   f'type `{self.command_string}help` to check available commands.'
+    #         await customMessages.system_message(ctx=ctx, c=Colour.red(), title=title, error_details=message)
+    #
+    #     elif isinstance(exception, commands.MissingRequiredArgument):
+    #         await customMessages.system_message(ctx=ctx, c=Colour.red(), title=f'Missing required argument',
+    #                                             error_details=f'{exception}')
 
     @commands.Cog.listener()
     async def on_command(self, ctx):
