d7e0067ff1fed412663c4b83337d7332fd210175
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-03-11 14:34:53 +0200

    Calculator package upgrade

--- xcash_dpops_api/dpopsCalculators.py
+++ xcash_dpops_api/dpopsCalculators.py
@@ -1,20 +1,18 @@
-#     # http://calculator.x-node.es/api/v1/delegates/twinkie -> get the roi for the delegate twinkie with the default values (2M xcash for 30 days)
-#     # http://calculator.x-node.es/api/v1/delegates/twinkie/20M -> get the roi for the delegate twinkie voting 20M with the default period (30 days)
-#     # http://calculator.x-node.es/api/v1/delegates/twinkie/20M/10 -> get the roi for the delegate twinkie voting 20M xcash during 10 days
-#
-
 import requests
 
 
 class DpopsCalculator:
     def __init__(self, delegate_name: str):
         self.delegate_name = delegate_name
-        self.api = "http://calculator.x-node.es/api/v1/delegates/"
+        self.api = "http://calculator.x-node.es/api/v1/delegates"
         self.calculator_api = self.api + '/' + self.delegate_name
 
-    def __process_request(self, api_link: str = None):
-        api_link = self.calculator_api + api_link
-        response = requests.get(api_link)
+    def __process_request(self,api_link=None):
+        if api_link:
+            api = self.calculator_api + api_link
+        else:
+            api = self.calculator_api
+        response = requests.get(api)
         if response.status_code == 200:
             return response.json()
         else:
