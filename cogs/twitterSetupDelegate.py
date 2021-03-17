f379ecae1086e2beea9a33928253843e3938a533
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2021-03-17 10:26:06 +0200

    :joystick: Discord Twitter setup cmd

--- cogs/twitterSetupDelegate.py
+++ cogs/twitterSetupDelegate.py
@@ -16,15 +16,12 @@ class TwitterSetupCommands(commands.Cog):
         elif status == 'off':
             return 0
 
-    async def stats_notifications_manager(self, ctx, setting_name: str, chn: TextChannel, status_code: int,
-                                          status: str, frame: str):
+    async def stats_notifications_manager(self, ctx, setting_name: str, status_code: int, funct: str, selection):
         if self.bot.setting.update_settings_by_dict(setting_name=setting_name,
-                                                    value={"status": int(status_code),
-                                                           "channel": int(chn.id)}):
-            await customMessages.system_message(ctx=ctx, c=Colour.green(), title=f"{frame} Stats Notifications",
-                                                error_details=f"You have successfully set/updated ***{frame} "
-                                                              f"statistic monitor*** on channel"
-                                                              f" ***{chn}*** to ***{status.upper()}***",
+                                                    value={"status": int(status_code)}):
+            await customMessages.system_message(ctx=ctx, c=Colour.green(), title=f"{funct} Twitter Notifications",
+                                                error_details=f"You have successfully set/updated settings to "
+                                                              f"***{selection.upper()}***",
                                                 destination=ctx.channel)
         else:
             await customMessages.system_message(ctx=ctx, c=Colour.red(), title="System Error",
@@ -46,7 +43,7 @@ class TwitterSetupCommands(commands.Cog):
                 {"name": "New block tweets",
                  "value": f"```{self.command_string}twitter block <on/off>```"},
                 {"name": "Dpops return rate showcase",
-                 "value": f"```{self.command_string}twitter reward <on/off>```"},
+                 "value": f"```{self.command_string}twitter rates <on/off>```"},
                 {"name": "Dpops Delegate Vote Initiative",
                  "value": f"```{self.command_string}twitter vote <on/off>```"},
                 {"name": ":warning: Warning ",
@@ -56,6 +53,58 @@ class TwitterSetupCommands(commands.Cog):
             ]
             await customMessages.embed_builder(ctx=ctx, c=Colour.green(), title=title, description=description,
                                                list_of_commands=list_of_commands)
+
+    @twitter.command()
+    async def daily(self, ctx, on_off: str):
+        status = on_off.lower()
+        if status and status in ["on", "off"]:
+            status_code = self.get_status_number(status=status)
+            await self.stats_notifications_manager(ctx=ctx, setting_name="t_del_daily",
+                                                   status_code=status_code, funct='Daily Delegate', selection=on_off)
+        else:
+            await customMessages.system_message(ctx=ctx, c=Colour.red(), title="Wrong Status",
+                                                error_details=f"You have provided wrong status. Available are"
+                                                              f" ***on/off*** while yours was {status}",
+                                                destination=ctx.channel)
+
+    @twitter.command()
+    async def block(self, ctx, on_off: str):
+        status = on_off.lower()
+        if status and status in ["on", "off"]:
+            status_code = self.get_status_number(status=status)
+            await self.stats_notifications_manager(ctx=ctx, setting_name="t_new_block",
+                                                   status_code=status_code, funct='New Block', selection=on_off)
+        else:
+            await customMessages.system_message(ctx=ctx, c=Colour.red(), title="Wrong Status",
+                                                error_details=f"You have provided wrong status. Available are"
+                                                              f" ***on/off*** while yours was {status}",
+                                                destination=ctx.channel)
+
+    @twitter.command()
+    async def rates(self, ctx, on_off: str):
+        status = on_off.lower()
+        if status and status in ["on", "off"]:
+            status_code = self.get_status_number(status=status)
+            await self.stats_notifications_manager(ctx=ctx, setting_name="t_return_rate",
+                                                   status_code=status_code, funct="ROI", selection=on_off)
+        else:
+            await customMessages.system_message(ctx=ctx, c=Colour.red(), title="Wrong Status",
+                                                error_details=f"You have provided wrong status. Available are"
+                                                              f" ***on/off*** while yours was {status}",
+                                                destination=ctx.channel)
+
+    @twitter.command()
+    async def vote(self, ctx, on_off):
+        status = on_off.lower()
+        if status and status in ["on", "off"]:
+            status_code = self.get_status_number(status=status)
+            await self.stats_notifications_manager(ctx=ctx, setting_name="t_call_to_vote",
+                                                   status_code=status_code, funct='Call to vote', selection=on_off)
+        else:
+            await customMessages.system_message(ctx=ctx, c=Colour.red(), title="Wrong Status",
+                                                error_details=f"You have provided wrong status. Available are"
+                                                              f" ***on/off*** while yours was {status}",
+                                                destination=ctx.channel)
 
 
 def setup(bot):
